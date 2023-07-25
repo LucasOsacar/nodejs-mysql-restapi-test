eb8c919cf2efa70debb5575cd79c02e6f023ee4c
LucasOsacar <Osacar994@gmail.com>
2023-07-25 16:02:12 -0300

    test update app

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,6 @@
 import { pool } from "../db.js";
 
 export const ping = async (req, res) => {
-    const [result] = await pool.query('SELECT "pong" as result');
+    const [result] = await pool.query('SELECT "pong pong" as result');
     res.json(result[0]);
 }
